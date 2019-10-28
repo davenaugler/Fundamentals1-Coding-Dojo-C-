9f9c8cc20cb6238453f0b629b7c5a857b0d956c8
Dave Naugler <davenaugler@gmail.com>
2019-10-28 11:29:28 -0600

    First C# assginment. Getting used to writting for loops, while loops, and if statments in C#

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Fundamentals1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Create a Loop that prints all value from 1-255
+            // for(int x = 1; x <= 255; x++)
+            // {
+            //     Console.WriteLine(x);
+            // }
+
+            // 2. Create a new loop that prints all the values from 1-100 that are divisible by 3 or 5, but not both
+            // for(int x = 1; x <= 100; x++)
+            // {
+            //     if(x % 3 == 0 || x % 5 == 0)
+            //     {
+            //         Console.WriteLine(x);
+            //     }
+            // }
+
+            // 3. Modify the previous loop to print "Fizz" for multiples of 3, "Buzz" for multiples of 5, and "FizzBuzz" for numbers that are multiples of both 3 and 5
+            // for(int x = 1; x <= 100; x++)
+            // {
+            //     if(x % 3 == 0 && x % 5 == 0)
+            //     {
+            //         Console.WriteLine("FizzBuzz");
+            //     }
+            //     else if(x % 5 == 0)
+            //     {
+            //         Console.WriteLine("Buzz");
+            //     }
+            //     else if(x % 3 == 0)
+            //     {
+            //         Console.WriteLine("Fizz");
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine(x);
+            //     }
+            // }
+
+            // 4. (Optional) If you used "for" loops for you solution, try doing the same with "while" loops. Vice-versa if you used "while" loops!
+            // int x = 1;
+            // while(x <= 100)
+            // {
+            //     if(x % 3 == 0 && x % 5 == 0)
+            //     {
+            //         Console.WriteLine("FizzBuzz");
+            //         x++;
+            //     }
+            //     else if(x % 5 == 0)
+            //     {
+            //         Console.WriteLine("Buzz");
+            //         x++;
+            //     }
+            //     else if(x % 3 == 0)
+            //     {
+            //         Console.WriteLine("Fizz");
+            //         x++;
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine(x);
+            //         x++;
+            //     }
+            // }
+
+
+        }
+    }
+}
